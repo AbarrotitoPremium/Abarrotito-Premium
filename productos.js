f49ab60938264131725b670491729e77f03bce21
AbarrotitoPremium <octavio.ibarra.rdz@gmail.com>
2025-02-06 23:44:08 -0700

    Update productos.js

--- productos.js
+++ productos.js
@@ -1,3 +1,19 @@
 const productos = [
-  { "TITULO": "Producto 1", "DESCRIPCION": "Desc", "PRECIO": 100 },
-  { "TITULO": "Producto 2", "DES
+    { 
+        "codigo": "44004", 
+        "nombre": "Producto 1", 
+        "descripcion": "Descripción del producto 1", 
+        "precio": 100,
+        "imagen": "img/producto1.jpg"
+    },
+    { 
+        "codigo": "44005", 
+        "nombre": "Producto 2", 
+        "descripcion": "Descripción del producto 2", 
+        "precio": 150,
+        "imagen": "img/producto2.jpg"
+    }
+];
+
+// Asegurar que los productos estén disponibles en la consola
+console.log("Productos cargados:", productos);
